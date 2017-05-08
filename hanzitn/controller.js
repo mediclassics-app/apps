fba587d4eafc5eec8a2684114f8b7a6694147598
Junho <Junho@kiom.re.kr>
2017-05-08 10:39:42 +0900

    update hanzitn

--- hanzitn/controller.js
+++ hanzitn/controller.js
@@ -36,6 +36,9 @@ angular.module("hanzitn", ["ngSanitize"])
 
 
 .controller("duplicationsMergeCtrl", function ($scope, $http, api, Board) {
+
+	$scope.rawTextdup = "[예시]\n不(U+F967) → 不(U+4E0D)\n更(U+F901) → 更(U+66F4)\n里(U+F9E9) → 里(U+91CC)\n六(U+F9D1) → 六(U+516D)"
+
 	$scope.mergeDuplications = function(){
 		$scope.dupProcessing = true;
 		var reqUrl = api.rooturl + 'duplications' //'?text=' + encodeURIComponent($scope.rawTextdup)
@@ -51,6 +54,9 @@ angular.module("hanzitn", ["ngSanitize"])
 })
 
 .controller("variantsMergeCtrl", function ($scope, $http, api, Board) {
+
+	$scope.rawTextvar = "[예시]\n尚(U+5C1A) → 尙(U+5C19)\n為(U+70BA) → 爲(U+7232)\n垒(U+5792) → 壘(U+58D8)\n胆(U+80C6) → 膽(U+81BD)"
+
 	$scope.tntype = $scope.tntype || "general"
 	$scope.mergeVariants = function(){
 		console.log($scope.tntype )
@@ -86,11 +92,29 @@ angular.module("hanzitn", ["ngSanitize"])
 .controller("footerCtrl", function($scope, api){
 	$scope.dictRefAddress = [
 		{"name": "duplications", 		"url": api.rooturl + "/dict/duplications" + "?type=tsv"},
-		{"name": "variants", 			"url": api.rooturl + "/dict/variants" + "?type=tsv"},
-		{"name": "variantsExtention", 	"url": api.rooturl + "/dict/variantsExtention" + "?type=tsv"}
+		{"name": "variants", 			"url": api.rooturl + "/dict/customvariants" + "?type=tsv"},
+		{"name": "variantsExtention", 			"url": api.rooturl + "/dict/customvariantsExtention" + "?type=tsv"},
+		{"name": "variants group", 			"url": api.rooturl + "/dict/variantsgroup" + "?type=tsv"},
+		{"name": "variantsExtention group", 	"url": api.rooturl + "/dict/variantsExtention" + "?type=tsv"}
 	]
 
 	$scope.externalRefs = [
-		{"name": "ctext",				"url": "http://ctext.org/dictionary.pl?if=en"}
+		{"name": "Unicode Character Code Charts",				"url": "http://www.unicode.org/charts/"},
+		{"name": "유니코드한자검색시스템",		  "url": "http://www.kostma.net/segment/segmentList.aspx"},
+		{"name": "유니코드한자검색기",		  "url": "http://db.koreanstudies.re.kr/"},
+		{"name": "Dictionary|Ctext",				"url": "http://ctext.org/dictionary.pl?if=en"},
+		{"name": "漢典",				"url": "http://www.zdic.net/"},
+		{"name": "이체자정보검색|한국고전번역원",		  "url": "http://db.itkc.or.kr/DCH/index.jsp"},
+		{"name": "고문서서체용례사전|한국학중앙연구원",		  "url": "http://www.kostma.net/segment/segmentList.aspx"},
+		{"name": "漢典書法",				"url": "http://sf.zdic.net/"},
+		{"name": "(敎育部)異體字字典",	"url": "http://dict2.variants.moe.edu.tw/variants/rbt/home.do"},
+		{"name": "國際電腦漢字及異體字知識庫",	"url": "http://chardb.iis.sinica.edu.tw/"},
+	]
+
+	$scope.externalTools = [
+		{"name": "Open Chinese Convert", 	"url": "https://github.com/BYVoid/OpenCC"},
+		{"name": "Make Me a Hanzi", 	"url": "https://github.com/skishore/makemeahanzi"},
+		{"name": "HanziJS", 	"url": "https://github.com/nieldlr/Hanzi"},
+		{"name": "Diff", 				"url": "https://neil.fraser.name/software/diff_match_patch/svn/trunk/demos/demo_diff.html"}
 	]
 })
